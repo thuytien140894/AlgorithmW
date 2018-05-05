1d8b7a3578557b0b9336efbf4820168a397a3291
Tien Ho <thuytien140894@gmail.com>
2018-05-04 17:28:00 -0700

    Defined type schemes.

--- src/Type.hs
+++ src/Type.hs
@@ -1,7 +1,15 @@
 module Type where 
 
-    data Type = TUnit          -- ^ TUnit
-              | Dyn            -- ^ Dynamic
+    import Data.Map (Map)
+    import qualified Data.Map as Map
+
+    type TVar = Type
+
+    data TypeScheme = ForAll [TVar] TypeScheme
+                    | Scheme Type
+                      deriving (Eq, Show)
+
+    data Type = TVar Int       -- ^ Type variable
               | Bool           -- ^ Boolean
               | Nat            -- ^ Natural numbers
               | Arr Type Type  -- ^ T->T 
